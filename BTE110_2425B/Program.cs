a31279b40271a7b521e3917624a131ae8bf6b83d
Gökhan Akçapınar <gokhana@hacettepe.edu.tr>
2025-04-26 07:40:46 +0300

    Add project files.

---
+++ BTE110_2425B/Program.cs
@@ -0,0 +1,716 @@
+﻿using System;
+
+namespace BTE110_2425B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tek satır
+
+            /*
+             
+                Birden
+                fazla
+                satır
+
+             */
+
+            //Console.WriteLine("1-" + Console.Title);
+
+            //Console.Title = "Merhaba Dünya";
+
+            //Console.WriteLine("2-" + Console.Title);
+
+            //Console.CapsLock = true;
+
+            /*
+            Console.WriteLine(Console.BackgroundColor);
+
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            Console.WriteLine(Console.BackgroundColor);
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("Merhaba Dünya!!");
+            */
+
+            //Console.CursorLeft = 10;
+            //Console.CursorTop = 3;
+
+            //Console.CursorVisible = true;
+
+            //Console.
+
+            //Console.WriteLine("Merhaba Dünya!!");
+
+            //Console.Write(Console.KeyAvailable);
+
+
+            //Console.WriteLine("1. Satır");
+            //Console.Write("\t");
+            //Console.Write("2. Satır");
+
+            //Console.Write();
+            //Console.WriteLine();
+
+            //Yorum satırı123432463521"""""
+
+            //string
+
+            //Console.WriteLine(true);
+            //Console.WriteLine("true");
+            //Console.WriteLine(1 < 0 && 1 == 1);
+            //Console.WriteLine(1 == 1);
+            //Console.WriteLine("1 == 1");
+            //Console.WriteLine(1 + 8);
+            //Console.WriteLine("1 + 8");
+            //Console.WriteLine("A");
+            //Console.WriteLine('A');
+            //Console.WriteLine('AB');
+            //Console.WriteLine("AB");
+
+            //Console.WriteLine((char)(((int)'A') + 1));
+
+            //Console.WriteLine('A' + 5);
+
+            //Console.WriteLine("A" + (5 + 1));
+
+            //Console.WriteLine("123" + 1);
+
+            //Console.WriteLine("Merhaba" + " " + "Dünya!");
+            //Console.WriteLine("1" + "2" + "3");
+
+            //Console.WriteLine(int.Parse("123") + 1);
+
+            //Console.WriteLine(123.3 + 123);
+
+            //Console.Beep(1000, 1000);
+
+            //Thread.Sleep(5000);
+
+            //Console.Clear();
+
+            //Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            //Console.WriteLine("Merhaba Dünya!!");
+
+            //Console.ResetColor();
+
+
+            //Console.SetCursorPosition(10, 10);
+            //Console.CursorLeft = 10;
+            //Console.CursorTop = 10;
+
+            //Console.WriteLine(Console.GetCursorPosition());
+
+            //Console.SetWindowPosition(10, 0);
+            //Console.SetWindowSize(5, 5);
+
+            //Console.ReadLine();
+
+
+
+            //int a = 5;
+
+            //Console.WriteLine(a);
+
+            //a = 6;
+
+            //int a = Console.Read();
+
+            //string a = Console.ReadLine();
+
+            //ConsoleKeyInfo a = Console.ReadKey(true);
+
+            //byte a = 255;
+
+            //short b = a;
+
+            //int c = b;
+
+            //long d = c;
+
+            //int e = (int)d;
+
+
+            //int x = 1;
+
+            //byte q = (byte)x;
+
+            //short w = (short)x;
+
+            //long r = x;
+
+
+            //short a = 257;
+
+            //byte b = (byte)a;
+
+            //byte c = Convert.ToByte(a);
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+            //string a = "Hello";
+
+            //int b = Convert.ToInt32(a);
+
+            //int c = int.Parse(a);
+
+
+            //Console.WriteLine(a + 1);
+            //Console.WriteLine(b + 1);
+            //Console.WriteLine(c + 1);
+
+            //Console.Write("Bir sayı giriniz: ");
+            //string sayi1 = Console.ReadLine();
+
+            //Console.WriteLine();
+
+            //Console.Write("Bir sayı giriniz: ");
+            //string sayi2 = Console.ReadLine();
+
+            //int toplam = int.Parse(sayi1) + int.Parse(sayi2);
+
+            //Console.WriteLine("Toplam: " + toplam);            
+            //Console.WriteLine("Toplam: {toplam}");
+            //Console.WriteLine($"Toplam: {toplam} - {toplam * 2}");
+            //Console.WriteLine("Toplam: {0}", toplam);
+
+            //double a = 257.99;
+            //double b = 10.6d;
+            //decimal c = 10.6m;
+            //float d = 10.6f;
+
+            ////byte e = (byte)Math.Round(a, 0);
+            //byte e = (byte)a;
+
+            //Console.WriteLine(e);
+
+
+            //27.03.2025
+
+            //Atama operatörleri
+
+            //int a = 5;
+
+            //int a, b;
+
+            //a = 1;
+
+            //b = a;
+
+            //a = 2;
+
+            //b = a;
+
+            //Console.WriteLine($"a: {a} - b: {b}");
+
+            //a = 1;
+
+            //a += 1; // a = a + 1;
+            //a /= 1; // a = a / 1;
+            //a -= 1; // a = a - 1;
+            //a *= 1; // a = a * 1;
+
+            //Matematiksel operatörler
+
+            //a = 1;
+            //b = 3;
+
+            //int c;
+
+            //c = a + b;
+            //c = a - b;
+            //c = a / b;
+            //c = a * b;
+            //c = a % b;
+
+            //++
+            //--
+
+            //a++;
+            //++a;
+            //a += 1;
+            //a = a + 1;
+
+            //Console.WriteLine($"a++: {a++}");
+            //Console.WriteLine($"++a: {++a}");
+
+            //a = 1;
+
+            //b = a++;
+
+            //Console.WriteLine($"a++: {a++}");
+
+            //Console.WriteLine($"a: {a}");
+
+
+            //Console.WriteLine(5 % 3);
+
+            //for (int i = 1; i <= 1000; i++)
+            //{
+            //if (i % 2 == 0)
+            //{
+            //    Console.WriteLine($"{i}: ÇİFT");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{i}: TEK");
+            //}
+
+            //if (i % 10 == 0)
+            //{
+            //    Console.Write($"{i}, ");
+            //}
+            //}
+
+            //Console.WriteLine(((1 + 2) * (3 + 1)) * 2);
+
+
+            //>
+            //<
+            //>=
+            //<=
+            //==
+            //!=
+
+            //!
+            //&&
+            //||
+
+            //bool a;
+
+            //a = 1 == 1;
+            //a = 1 != 1;
+            //a = 1 > 1;
+            //a = 1 >= 1;
+            //a = 1 < 1;
+            //a = 1 <= 1;
+
+            //int x, y;
+            //bool z;
+
+            //x = 3;
+            //y = 4;
+
+            //Console.WriteLine($"x == y: {x == y}");
+            //Console.WriteLine($"x != y: {x != y}");
+            //Console.WriteLine($"x > y: {x > y}");
+            //Console.WriteLine($"x >= y: {x >= y}");
+            //Console.WriteLine($"x < y: {x < y}");
+            //Console.WriteLine($"x <= y: {x <= y}");
+
+            //Console.WriteLine("--------------------");
+
+            //Console.WriteLine($"true && true: {true && true}");
+            //Console.WriteLine($"true && false: {true && false}");
+            //Console.WriteLine($"false && true: {false && true}");
+            //Console.WriteLine($"false && false: {false && false}");
+
+            //Console.WriteLine("--------------------");
+
+            //Console.WriteLine($"true || true: {true || true}");
+            //Console.WriteLine($"true || false: {true || false}");
+            //Console.WriteLine($"false || true: {false || true}");
+            //Console.WriteLine($"false || false: {false || false}");
+
+            //Console.WriteLine("--------------------");
+
+            //Console.WriteLine($"!true: {!true}");
+            //Console.WriteLine($"!false: {!false}");
+
+            //Console.WriteLine(((true && false) && (true && false)));
+            //Console.WriteLine(!((true && false) && (true && false)));
+            //Console.WriteLine(((true || false) || (true || false)));
+            //Console.WriteLine(((true || false) && (true || false)));
+            //Console.WriteLine(((true && false) || (true || false)));
+            //Console.WriteLine(((true && false) && (true || false)));
+
+
+            //Console.WriteLine("--------------------");
+
+            //Console.Write("K Adı: ");
+            //string kAd = Console.ReadLine();
+
+            //Console.Write("Şifre: ");
+            //string kSifre = Console.ReadLine();
+
+            //Console.WriteLine(kAd == "ahmet" || kSifre == "123");
+
+
+            //if (true)
+            //{
+
+            //}
+            //else if (true)
+            //{
+
+            //}
+            //else if (true)
+            //{
+
+            //}
+            //else
+            //{
+
+            //}
+
+            //if (true)
+            //{
+
+            //}
+
+            //if (true)
+            //{
+
+            //}
+
+            //if (true)
+            //{
+
+            //}
+
+            //if (true)
+            //{
+            //    if (true)
+            //    {
+            //        if (true)
+            //        {
+
+            //        }
+            //        else if (true)
+            //        {
+
+            //        }
+            //    }
+            //    else
+            //    {
+
+            //    }
+            //}
+
+            //if (true)
+            //{
+
+            //}
+
+            ////true ? "" : "";
+
+            ////if (true)
+
+            //if (true && false || false && true)
+            //{
+
+            //}
+
+            //if (!true)
+            //{
+
+            //}
+
+
+            //while (true)
+            //{
+
+            //}
+
+            //Console.Write("1");
+
+            //if (false)
+            //{
+            //    if (true)
+            //    {
+            //        if (false)
+            //        {
+            //            if (true)
+            //            {
+            //                Console.Write("2");
+            //            }
+            //        }
+            //    }
+            //}
+
+            //if ((true || false) && (true || true))
+            //{
+            //    Console.Write("3");
+            //}
+
+            //Console.Write("4");
+
+            //Random rnd = new Random();
+
+            //int a = 0;
+
+            //while (a++ < 10)
+            //{
+            //    int b = rnd.Next(1,9);
+
+            //    if (b % 2 == 0)
+            //    {
+            //        Console.WriteLine($"{a} - {b} - Çift");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{a} - {b} - Tek");
+            //    }
+            //}
+
+            //Console.WriteLine(Console.KeyAvailable == false);
+
+            //while (Console.KeyAvailable == false)
+            //{
+            //    Console.WriteLine("TEST");
+            //}
+
+            //while (!Console.KeyAvailable)
+            //{
+            //    Console.Write("TEST");
+            //}
+
+            //while (true)
+            //{
+            //    Console.Write("Bir sayı gir (0-100): ");
+            //    string a = Console.ReadLine();
+
+            //    if (a == "99")
+            //    {
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Girdiğiniz sayı: {a}");
+            //    }
+
+            //    //break;
+            //    //continue;
+
+            //    //Console.WriteLine("Güle güle...");
+            //}
+
+            //int a = 0;
+
+            //int toplam = 0;
+
+            //while (a <= 100)
+            //{
+
+            //    if (a % 2 == 0)
+            //    {
+            //        toplam += a;
+            //    }
+
+            //    a++;
+            //}
+
+            //Console.WriteLine($"Toplam: {toplam}");
+
+
+            //Random rnd = new Random();
+
+            //while (true)
+            //{
+            //    Console.Write(rnd.Next(1111, 9999));
+            //}
+
+
+
+
+            //
+
+            //switch (switch_on)
+            //{
+            //    default:
+            //}
+
+            //if (true)
+            //{
+
+            //}
+            //else
+            //{
+
+            //}
+
+            //while (true)
+            //{
+
+            //}
+
+            //Console.WriteLine("Sayı: " + 15);
+
+            //int a = 256;
+            //byte b = (byte)a;
+
+            //Console.WriteLine($"a: {b}");
+
+            //if (b == false)
+            //{
+
+            //}
+
+            //Console.Write("Bir sayı girin: ");
+
+            //string sayi = Console.ReadLine();
+
+            //int sayiInt = 0;
+
+            //try
+            //{
+            //    sayiInt = Convert.ToInt32(sayi);
+            //    Console.WriteLine($"Girilen sayı: {sayiInt}");
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Uygun bir sayı girmediniz...");
+            //}
+
+
+            //24.04.2025
+            //Diziler
+            //For döngüsü
+
+            //int a1;
+            //int a2;
+            //int a3;
+            //int a4;
+            //int a5;
+
+            //int[] a = new int[5];
+
+            //int[] b = { 1, 2, 3, 4, 5 };
+
+            //int[] c;
+
+
+            //int diziBoyutu = 10;
+
+            //c = new int[diziBoyutu];
+
+            //int[] a = new int[6];
+
+            //a[0] = 1;
+            //a[1] = 2;
+            //a[2] = 3;
+            //a[3] = 4;
+            //a[4] = 5;
+            //a[5] = 6;
+
+            ////a[0] += 1;
+            ////a[0]++;
+
+            //if (a[0] >= 0) { 
+
+            //}
+
+            ////while (true)
+            ////{
+
+            ////}
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    a[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+            //string[] a = { "Ankara", "İstanbul", "İzmir" };
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write(a[i]);
+            //}
+
+            //int i = 0;
+
+            //while (i < a.Length)
+            //{
+            //    Console.Write(a[i]);
+
+            //    i++;
+            //}
+
+            //int[] a = { 1, 2, 3, 4, 5 };
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+            //Console.WriteLine();
+
+            //for (int i = a.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    //Console.WriteLine($"i: {i}");
+
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        //Console.WriteLine($"\tj: {j}");
+            //        Console.WriteLine($"{i} * {j} = {i*j}");
+            //    }
+
+            //    Console.WriteLine("--------------");
+
+            //}
+
+
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    for (int j = 0; j < i + 1; j++)
+            //    {
+            //        Console.Write("#");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            int[] notlar;
+
+            Console.Write("Kaç tane not gireceksiniz: ");
+
+            string diziBoyutStr = Console.ReadLine();
+
+            int diziBoyutInt = Convert.ToInt32(diziBoyutStr);
+
+            notlar = new int[diziBoyutInt];
+
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                Console.Write($"{i + 1}. Notu gir: ");
+                string notStr = Console.ReadLine();
+                notlar[i] = Convert.ToInt32(notStr);
+            }
+
+            int toplamNot = 0;
+
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                toplamNot += notlar[i];
+            }
+
+            double ortalama = (double)toplamNot / notlar.Length;
+
+            Console.WriteLine("#######################");
+
+            Console.WriteLine($"Notların Toplamı: {toplamNot}");
+            Console.WriteLine($"Notların ortalaması: {ortalama}");
+
+
+
+        }
+
+    }
+}
